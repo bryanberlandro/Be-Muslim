45e7d0e6fe4af097e8335c0145529a19d006248b
Berlandro <bryangodlive0@gmail.com>
2024-04-17 22:18:37 +0700

    feat: search at quran page

--- src/components/elements/InputSearch.jsx
+++ src/components/elements/InputSearch.jsx
@@ -1,12 +1,13 @@
 import { FaSearch } from "react-icons/fa";
 
-export function InputSearch(){
+export function InputSearch({onChange}){
     return(
         <div className="flex items-center bg-white rounded-full w-full px-6 py-2 justify-between text-sm shadow-sm border-2 border-emerald-100 transition-all duration-100 group focus-within:border-emerald-400 focus-within:scale-[1.02]">
                 <input 
                 type="text" 
                 placeholder="Al-fatihah, Al-Mulk" 
-                className="outline-none border-none w-[80%] "
+                className="outline-none border-none w-[80%]"
+                onChange={onChange}
                 />
                 <FaSearch className="text-emerald-600"/>
         </div>
--- src/pages/quran.jsx
+++ src/pages/quran.jsx
@@ -4,6 +4,7 @@ import { Loader } from "../components/elements/Loader";
 import axios from "axios";
 import SurahCard from "../components/fragments/SurahCard";
 import { Navbar } from "../components/layout/Navbar";
+import { Footer } from "../components/layout/home/Footer";
 
 
 export default function QuranPage(){
@@ -15,6 +16,8 @@ export default function QuranPage(){
     const [activeTab, setActiveTab] = useState(0);
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true)
+    const [searchRes, setSearchRes] = useState([])
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         async function fetchData(){
@@ -35,15 +38,26 @@ export default function QuranPage(){
         setActiveTab(index)
     }
 
-    console.log(activeTab)
+    function handleSearch(e){
+        const inputValue = e.target.value.toLowerCase()
+        setSearchTerm(inputValue)
+
+        const filteredData = data.filter((item) => 
+            item.name.transliteration.en.toLowerCase().includes(inputValue)
+        )
+        setSearchRes(filteredData)
+    }
+
 
     return(
         <>
         <Navbar/>
-        <div className="pt-nav px-[5%]">
-            <InputSearch/>
+        <div className="pt-nav px-[5%] pb-10">
+            <InputSearch
+            onChange={handleSearch}
+            />
             <div className="mt-6 flex border-b-2 border-neutral-100 py-2 text-sm font-semibold relative">
-                <div className={`absolute transition-all duration-150 h-1 w-1/3  bg-emerald-300 bottom-0 rounded-full translate-x-[${tabs[activeTab].translate}%]`}></div>
+                <div className={`absolute transition-all duration-300 h-1 w-1/3  bg-emerald-300 bottom-0 rounded-full translate-x-[${tabs[activeTab].translate}%]`}></div>
                 {
                     tabs.map((tab,index) => (
                         <div 
@@ -60,21 +74,23 @@ export default function QuranPage(){
             <div className={`flex flex-col mt-2 ${loading ? 'h-96 justify-center items-center' : ''}`}>
                 {
                     loading 
-                    ?   <Loader/>
-                    :   data.map(item => (
-                        <SurahCard
-                            key={item.number}
-                            ayat={item.name.short}
-                            number={item.number}
-                            surah={item.name.transliteration.en}
-                            total={item.numberOfVerses}
-                            translate={item.name.translation.en}
-                        />
+                    ?   (<Loader/>)
+                    :   (
+                        (searchTerm ? searchRes : data).map(item => (
+                            <SurahCard
+                                key={item.number}
+                                ayat={item.name.short}
+                                number={item.number}
+                                surah={item.name.transliteration.en}
+                                total={item.numberOfVerses}
+                                translate={item.name.translation.en}
+                            />
                         ))
+                        )
                 }
             </div>
-
         </div>
+        <Footer/>
         </>
     )
 }
