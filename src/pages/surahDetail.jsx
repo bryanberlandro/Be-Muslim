c01c2ddac9b1e835471d0252b18db37b31f64c4e
Berlandro <bryangodlive0@gmail.com>
2024-04-20 00:40:35 +0700

    feat: add list ayah at quran page

--- src/pages/surahDetail.jsx
+++ src/pages/surahDetail.jsx
@@ -2,13 +2,14 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { FaChevronDown } from "react-icons/fa"
 import { useParams } from "react-router-dom"
+import { FiBookOpen, FiBookmark, FiPlay } from "react-icons/fi";
 
 export default function SurahDetailPage(){
     const { id, surahName } = useParams()
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
     const [showMore, setShowMore] = useState(false)
-    
+
     useEffect(() => {
         async function fetchData(){
             try {
@@ -22,6 +23,7 @@ export default function SurahDetailPage(){
         
         fetchData()
     }, [])
+
 
     function handleShowMore(){
         setShowMore(!showMore)
@@ -51,6 +53,7 @@ export default function SurahDetailPage(){
         </div>
         <div className="pt-36 px-[5%]">
             <h1 className="font-medium text-lg">{data.number}</h1>
+
             <div className="text-center space-y-3">
                 <h1 className="text-3xl font-bold">{data?.name.long}</h1>
                 <div>
@@ -58,21 +61,8 @@ export default function SurahDetailPage(){
                     <p className="text-sm text-neutral-500">( {data.name.translation.en} )</p>
                 </div>
             </div>
+
             <div className="text-sm mt-10">
-                <div className="rounded-lg bg-emerald-500 text-white px-5 py-2 flex justify-between divide-x-2 divide-emerald-200 shadow-sm">
-                    <div className="space-y-1 w-1/3 text-center">
-                        <h1 className="text-xs text-neutral-100">Revelation</h1>
-                        <p className="font-medium">{data.revelation.id}</p>
-                    </div>
-                    <div className="space-y-1 w-1/3 text-center">
-                        <h1 className="text-xs text-neutral-100">Sequence</h1>
-                        <p className="font-medium">{data.sequence}</p>
-                    </div>
-                    <div className="space-y-1 w-1/3 text-center">
-                        <h1 className="text-xs text-neutral-100">Verses</h1>
-                        <p className="font-medium">{data.numberOfVerses}</p>
-                    </div>
-                </div>
                 <div className="mt-5">
                     <h1 className="text-emerald-600 font-semibold text-base">Tafsir</h1>
                     <p>
@@ -89,7 +79,46 @@ export default function SurahDetailPage(){
                         </span>
                     </p>
                 </div>
+                <div className="rounded-lg bg-emerald-500 text-white px-5 py-3 flex justify-between divide-x-2 divide-emerald-200 shadow-sm mt-8">
+                    <div className="space-y-1 w-1/3 text-center">
+                        <h1 className="text-xs text-neutral-100">Revelation</h1>
+                        <p className="font-medium">{data.revelation.id}</p>
+                    </div>
+                    <div className="space-y-1 w-1/3 text-center">
+                        <h1 className="text-xs text-neutral-100">Sequence</h1>
+                        <p className="font-medium">{data.sequence}</p>
+                    </div>
+                    <div className="space-y-1 w-1/3 text-center">
+                        <h1 className="text-xs text-neutral-100">Verses</h1>
+                        <p className="font-medium">{data.numberOfVerses}</p>
+                    </div>
+                </div>
+                
             </div>
+
+            <div className="flex flex-col mt-5">
+            {
+                data.verses?.map(ver => (
+                <div key={ver.number.inSurah} className="border-b-2 px-4 py-4">
+                    <div className="text-sm flex justify-between items-center font-medium">
+                        <div className="flex items-center gap-5">
+                            <h1>{ver.meta.juz} : {ver.number.inQuran}</h1>
+                            <FiBookOpen/>
+                            <FiPlay/>
+                        </div>
+                        <FiBookmark/>
+                    </div>
+                    <div className="text-right py-5 text-xl">
+                        <h1>{ver.text.arab}</h1>
+                    </div>
+                    <div className="text-sm">
+                        <h1>{ver.text.transliteration.en}</h1>
+                    </div>
+                </div>
+                ))
+            }
+            </div>
+
         </div>
         </>
     )
